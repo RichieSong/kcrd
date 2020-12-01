d1ac8e50e776cbd785acd58ead0a2911e7578b7d
richie.song <421510020@qq.com>
2020-12-01 23:52:31 +0800

    kcrd

--- controller.go
+++ controller.go
@@ -1,8 +1,10 @@
-package kcrd
+package main
 
 import (
 	"fmt"
+	"go/src/kcrd/pkg/apis/stable/v1beta1"
 	informer "go/src/kcrd/pkg/client/informers/externalversions/stable/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/cache"
@@ -17,10 +19,18 @@ type Controller struct {
 }
 
 func NewController(informer informer.CronTabInformer) *Controller {
-	return &Controller{
+	c := &Controller{
 		queue:    workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "crontab-controller"),
 		informer: informer,
 	}
+	klog.Infof("Setting up crontab controller")
+	// 注册监听事件
+	informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    c.onAdd,
+		UpdateFunc: c.onUpdate,
+		DeleteFunc: c.onDelete,
+	})
+	return c
 }
 func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
 	defer runtime.HandleCrash()
@@ -32,6 +42,8 @@ func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
 		return fmt.Errorf("timeout waiting for caches to sync")
 	}
 
+	klog.Info("cache sync success")
+
 	for i := 0; i < threadiness; i++ {
 		go wait.Until(c.runWorker, time.Second, stopCh)
 	}
@@ -42,17 +54,89 @@ func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
 }
 
 func (c *Controller) runWorker() {
-	//for
 }
 
 // 实现业务逻辑
-
-func (c *Controller)processNextItem()bool  {
-	key,quit:= c.queue.Get()
-	if quit{
+func (c *Controller) processNextItem() bool {
+	obj, shutdown := c.queue.Get()
+	if shutdown {
 		return false
 	}
-	defer c.queue.Done(key)
-	//err:=c.syncTo
+	// 告诉队列 我们已经完成处理的此key
+	// 这将为其他worker解锁该key
+	// 这将确保安全的并行处理，因为永远不会并行处理具有相同key的两个pod
+	defer c.queue.Done(obj)
+	// 调用业务逻辑处理方法
+	err := func(obj interface{}) error {
+		// 告诉队列 我们已经完成处理的此key
+		// 这将为其他worker解锁该key
+		// 这将确保安全的并行处理，因为永远不会并行处理具有相同key的两个pod
+		defer c.queue.Done(obj)
+		var ok bool
+		var key string
+		if key, ok = obj.(string); !ok {
+			c.queue.Forget(obj)
+			return fmt.Errorf("expected string in workqueueu but get %v", obj)
+		}
+		// 业务逻辑处理
+		if err := c.syncHandler(key); err != nil {
+			return fmt.Errorf("sync error: %v", err)
+		}
+		c.queue.Forget(obj)
+		klog.Info("successfully syncd %s", key)
+		return nil
+	}(obj)
+	if err != nil {
+		runtime.HandleError(err)
+	}
 	return true
+}
+
+// key - > crontab - > indexer
+func (c *Controller) syncHandler(key string) error {
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return err
+	}
+	// 获取crontab 实际上是从indexer获取的
+	crontab, err := c.informer.Lister().CronTabs(namespace).Get(name)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// 对于的crontab对象已经被删除了
+			klog.Warningf("Crontab deleting: %s/%s", namespace, name)
+			return nil
+		}
+		return err
+	}
+	klog.Info("Crontab try to process: %#v ..", crontab)
+	return nil
+}
+
+func (c *Controller) onAdd(obj interface{}) {
+	key, err := cache.MetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.queue.AddRateLimited(key)
+
+}
+func (c *Controller) onUpdate(newobj, oldobj interface{}) {
+
+	// 判断资源版本是否一致 ，如果一致，不更新
+	old := oldobj.(*v1beta1.CronTab)
+	newo := newobj.(*v1beta1.CronTab)
+	if old.ResourceVersion == newo.ResourceVersion {
+		return
+	}
+	c.onAdd(newo)
+
+}
+func (c *Controller) onDelete(obj interface{}) {
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.onAdd(key)
 }
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package kcrd
+package main
 
 import (
 	"flag"
@@ -17,11 +17,13 @@ import (
 	"path/filepath"
 	"sync"
 )
+
 var config *rest.Config
 var (
 	clsMap  = make(map[string]*kubernetes.Clientset)
 	clsLock = &sync.Mutex{}
 )
+
 func CLS(env string) *kubernetes.Clientset {
 	clsLock.Lock()
 	defer clsLock.Unlock()
@@ -37,8 +39,6 @@ func buildConfigFromFlagContext(context string, kubeconfigPath string) (*rest.Co
 		}).ClientConfig()
 }
 
-
-
 func InitK8s() {
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
@@ -52,7 +52,7 @@ func InitK8s() {
 	// config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	clsLock.Lock()
 	defer clsLock.Unlock()
-	for _, env := range []string{"PRE","DEV"} {
+	for _, env := range []string{"pre", "dev"} {
 		config, err = buildConfigFromFlagContext(env, *kubeconfig)
 		if err != nil {
 			panic(err.Error())
@@ -68,40 +68,44 @@ func InitK8s() {
 	}
 }
 
+// 优雅退出
 func setupSignalHander() (stopCh <-chan struct{}) {
 	stop := make(chan struct{})
-	c:= make(chan os.Signal,2)
-	signal.Notify(c,[]os.Signal{os.Interrupt,syscall.SIGTERM}...)
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, []os.Signal{os.Interrupt, syscall.SIGTERM}...)
 	go func() {
 		<-c
 		close(stop)
 		<-c
 		os.Exit(1)
 	}()
-	return
+	return stop
 }
 func main() {
 	flag.Parse()
-	//_ := CLS("PRE")
+
 	InitK8s()
-	klog.Info(config)
-	// 实例化crontab
-	crontabClientSet,err:=versioned.NewForConfig(config)
-	if err!=nil{
-		klog.Fatalf( "Error init kubernetes contab client: %s",err.Error())
+	//klog.Info(config)
+	// 全局clientset
+	//clientset := CLS("dev")
+
+	// 实例化crontab clientset
+	crontabClientSet, err := versioned.NewForConfig(config)
+	if err != nil {
+		klog.Fatalf("Error init kubernetes contab client: %s", err.Error())
 	}
-	stopCh :=setupSignalHander()
+	stopCh := setupSignalHander()
 	// 实例化crontab former工厂类
-	sharedInformerFactory:=externalversions.NewSharedInformerFactory(crontabClientSet,time.Minute)
-	// 启动informer 监听list
-	go sharedInformerFactory.Start(stopCh)
+	sharedInformerFactory := externalversions.NewSharedInformerFactory(crontabClientSet, time.Minute)
 
 	// 实例化crontab控制器
+	controller := NewController(sharedInformerFactory.Stable().V1beta1().CronTabs())
 
-	controller:= NewController(sharedInformerFactory.Stable().V1beta1().CronTabs())
+	// 启动informer 监听list and watch  watch之前务必注册informer
+	go sharedInformerFactory.Start(stopCh)
+
 	// 启动控制器循环
-	if err:= controller.Run(1,stopCh);err!=nil{
-		klog.Fatalf("%s",err.Error)
+	if err := controller.Run(1, stopCh); err != nil {
+		klog.Fatalf("%s", err.Error)
 	}
 }
-
